c33eb798e997fe99813901d75051e11e213ec483
Ali Sampson <alisampson2@gmail.com>
2023-10-20 14:38:57 +0000

    Project screenshots added

--- lib/app/utils/project_utils.dart
+++ lib/app/utils/project_utils.dart
@@ -43,10 +43,25 @@ List<ProjectUtils> projectUtils = [
   ProjectUtils(
     banners: 'assets/imgs/04.png',
     icons: 'assets/imgs/flutter.png',
+    titles: 'FLY NYC',
+    description:
+        'This is a Tour Booking App via Aircraft, the link is also available, check below.',
+    links: 'https://play.google.com/store/apps/details?id=com.flynyc.app',
+  ),
+  ProjectUtils(
+    banners: 'assets/imgs/05.png',
+    icons: 'assets/imgs/flutter.png',
     titles: 'Akwaaba - Educare',
     description:
-        'This is a just HR Management App UI by using flutter, the link is also available, check below.',
-    links:
-        'https://play.google.com/store/apps/details?id=com.akwaaba.app.educare',
+        'This is a HR Management App for educational institutions by using flutter, the link is also available, check below.',
+    links: 'https://apps.apple.com/us/app/edu-care/id6450746311',
+  ),
+  ProjectUtils(
+    banners: 'assets/imgs/06.png',
+    icons: 'assets/imgs/flutter.png',
+    titles: 'Akwaaba',
+    description:
+        'This is a  HR Management App for businesses, the link is also available, check below.',
+    links: 'https://apps.apple.com/us/app/akwaaba/id6445876174',
   ),
 ];
